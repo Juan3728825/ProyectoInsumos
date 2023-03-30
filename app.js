6bec56ceb8d8feb671e383d661e476838ec26984
Juan3728825 <jmmartinez4817@misena.edu.co>
2023-03-30 16:23:23 -0500

    Juan vistas

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
+const app = express();
+
+//prueba
+// Establecer la carpeta pública
+app.use('/resources', express.static('public'));
+
+// Establecer el motor de plantillas
+app.set('view engine', 'ejs');
+
+// Middleware para procesar datos de formulario
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Agregar las rutas definidas en el módulo router
+app.use('/', router);
+
+router.get('/rol', (req, res) => {
+  const title = "Selección de Rol";
+  res.render('Rol', { title });
+});
+
+app.get('/', function(req, res) {
+  res.render('rol');
+});
+
+app.get('/registroaprendiz', function(req, res) {
+  res.render('registroaprendiz');
+});
+
+app.get('/registroinstructor', function(req, res) {
+  res.render('registroinstructor');
+});
+
+app.get('/registrocoordinador', function(req, res) {
+  res.render('registrocoordinador');
+});
+
+app.get('/registroadministrador', function(req, res) {
+  res.render('registroadministrador');
+});
+
+app.get('/devolucionInsumos', function(req, res) {
+  res.render('devolucionInsumos');
+});
+
+app.get('/inventario', function(req, res) {
+  res.render('inventario');
+});
+// Redirigir si la url no está definida
+
+router.post('/registroinstructor', function(req, res) {
+  // Código para registrar el instructor
+  // ...
+  // Enviar respuesta al cliente
+  res.send('Registro exitoso');
+});
+
+router.post('/registrocoordinador', function(req, res) {
+  // Código para registrar el coordinador
+  // ...
+  // Enviar respuesta al cliente
+  res.send('Registro exitoso');
+});
+
+router.post('/registroaprendiz', function(req, res) {
+  // Código para registrar el coordinador
+  // ...
+  // Enviar respuesta al cliente
+  res.send('Registro exitoso');
+});
+
+router.post('/registroadministrador', function(req, res) {
+  // Código para registrar el coordinador
+  // ...
+  // Enviar respuesta al cliente
+  res.send('Registro exitoso');
+});
+// Redirigir si la url no está definida
+
+
+
+
+
---
+++ router.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+
+// vista principal de la pagina
+router.get('/', (req,res)=>{
+    res.render('index.ejs');
+})
+//Esto es una prueba
+
+// VISTAS INSTRUCTOR
+
+router.get('/ambientes', (req,res)=>{
+    res.render('ambientes.ejs');
+})
+
+router.get('/controlAula', (req,res)=>{
+    res.render('controlAula.ejs');
+})
+
+  router.get('/herramientas', (req, res) => {
+    res.render('herramientas.ejs');
+  });
+
+  router.get('/material', (req, res) => {
+    res.render('material.ejs');
+  });
+
+  router.get('/menu-instructor', (req, res) => {
+    res.render('menu-instructor.ejs');
+  });
+  
+  router.get('/registroinstructor', (req, res) => {
+    res.render('registroinstructor.ejs');
+  });
+  
+  router.get('/registrocoordinador', (req, res) => {
+      res.render('registrocoordinador.ejs');
+    });
+  
+      // VISTAS COORDINADOR
+  router.get('/aprobar', (req, res) => {
+    res.render('aprobar.ejs');
+  });
+  router.get('/reportes', (req, res) => {
+    res.render('reportes.ejs');
+  });
+
+  router.get('/seguimiento', (req, res) => {
+    res.render('seguimiento.ejs');
+  });
+
+  // VISTAS APRENDIZ
+
+  router.get('/index', (req, res) => {
+    res.render('index.ejs');
+  });
+  router.get('/prestamoPC1', (req, res) => {
+    res.render('prestamoPC1.ejs');
+  });
+  router.get('/prestamoPC2', (req, res) => {
+    res.render('prestamoPC2.ejs');
+  });
+  router.get('/prestamoPC3', (req, res) => {
+    res.render('prestamoPC3.ejs');
+  });
+  router.get('/menu-aprendiz', (req, res) => {
+    res.render('menu-aprendiz.ejs');
+  });
+
+
+  //VISTAS ADMINISTRADOR
+
+  router.get('/rol', (req, res) => {
+    res.render('rol');
+  });
+
+  router.get('/registroaprendiz', (req, res) => {
+    res.render('registroaprendiz');
+  });
+  
+  router.get('/registroinstructor', (req, res) => {
+    res.render('registroinstructor');
+  });
+  
+  router.get('/registrocoordinador', (req, res) => {
+      res.render('registrocoordinador');
+    });
+  
+    router.get('/registroadministrador', (req, res) => {
+      res.render('registroadministrador');
+    });
+  
+    router.get('/devolucionInsumos', (req, res) => {
+      res.render('devolucionInsumos');
+    });
+  
+    router.get('/inventario', (req, res) => {
+      res.render('inventario');
+    });
+
+
+module.exports = router;
